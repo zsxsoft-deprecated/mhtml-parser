49808368b3e78a75eb61b9b273b7e3a373c27d48
zsx <zsx@zsxsoft.com>
2016-01-02 22:34:50 +0800

    Add test to 100% coveralls

--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@ var deepDiff = require("deep-diff");
 describe('Simple Test', function() {
   var json = require("./simple/simple.json");
   describe('Load file by parser', function () {
-    it('json should equal', function (done) {
+    it('json should be equal', function (done) {
       parser.loadFile(__dirname + "/simple/simple.mht", "gbk", function(err, data) {
       	if (err) throw err;
       	done(deepDiff(data, json));
@@ -17,9 +17,25 @@ describe('Simple Test', function() {
   });
 
   describe('Load file by user', function () {
-    it('json should equal', function (done) {
+    it('json should be equal', function (done) {
       var data = parser.parse(iconv.decode(require("fs").readFileSync(__dirname + "/simple/simple.mht", null), "gbk"));
       done(deepDiff(data, json));
     });
   });
 });
+
+describe('error-versionNoSupported', function() {
+  describe('Test', function () {
+    it('Should throw error', function (done) {
+      parser.loadFile(__dirname + "/error-versionNoSupported/test.mht", "gbk", function(err, data) {
+      	if (err) {
+      		done(err == "Unsupported version");
+      	} else {
+      		throw ("Didn't throw error!");
+      	}
+      });
+    });
+  });
+
+});
+
